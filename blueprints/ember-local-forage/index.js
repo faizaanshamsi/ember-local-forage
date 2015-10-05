9e094f83c5d8626893254c82a287e57946bced2a
Eric Kelly <heroiceric@gmail.com>
2015-10-05 11:01:41 -0400

    Fix syntax error in generator

--- blueprints/ember-local-forage/index.js
+++ blueprints/ember-local-forage/index.js
@@ -1,9 +1,4 @@
 module.exports = {
-  description: 'Who knows'
-
-  normalizeEntityName: function() {
-  },
-
   afterInstall: function(options) {
     return this.addBowerPackagesToProject([
       { name: 'localforage', target: '~1.12.0' }
