4b38f2f696b04534c989a04151807a33bef3d416
faizaanshamsi <faizaanshamsi@gmail.com>
2015-10-05 11:16:29 -0400

    fix entity name

--- blueprints/ember-local-forage/index.js
+++ blueprints/ember-local-forage/index.js
@@ -1,4 +1,9 @@
 module.exports = {
+  description: 'Add localForage to Ember',
+
+  normalizeEntityName: function() {
+  },
+
   afterInstall: function(options) {
     return this.addBowerPackagesToProject([
       { name: 'localforage', target: '~1.12.0' }
